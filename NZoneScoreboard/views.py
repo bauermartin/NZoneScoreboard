48200089c3e8e9f89b2417379a7d87be933793e2
bauer <bauer@DESKTOP-O6HJVDC>
2023-05-22 23:14:48 +0200

    Current match by ID

--- NZoneScoreboard/views.py
+++ NZoneScoreboard/views.py
@@ -2,36 +2,58 @@
 Routes and views for the flask application.
 """
 
+from asyncio.windows_events import NULL
+import requests
+import json
 from datetime import datetime
 from flask import render_template
+from flask import request
 from NZoneScoreboard import app
 
-@app.route('/')
-@app.route('/home')
-def home():
-    """Renders the home page."""
+@app.route('/nc/scoreboard/<id>')
+def scoreboard(id):
+    """Renders the scoreboard page."""
+    print("Open Scoreboard")
+    match = getCurrentMatch(id)
+    print(match)
+    #getCurrentlyLoggedIn()
     return render_template(
-        'index.html',
-        title='Home Page',
-        year=datetime.now().year,
+        'scoreboard.html',
+        match=match
     )
 
-@app.route('/contact')
-def contact():
-    """Renders the contact page."""
-    return render_template(
-        'contact.html',
-        title='Contact',
-        year=datetime.now().year,
-        message='Your contact page.'
-    )
 
-@app.route('/about')
-def about():
-    """Renders the about page."""
-    return render_template(
-        'about.html',
-        title='About',
-        year=datetime.now().year,
-        message='Your application description page.'
-    )
+def getCurrentMatch(id):
+    r = requests.get("https://new-chapter.eu/app.php/nczone/api/matches/running")
+    matches = r.json()
+    out = {}
+    match = findMatch(id, matches)
+    out['team1'] = []
+    out['team2'] = []
+    for p in match["players"]["team1"]:
+        out['team1'].append(p)
+    for p in match["players"]["team2"]:
+        out['team2'].append(p)
+    out['teamsize'] = len(out['team1'])
+    return out
+
+def findMatch(id, matches):
+    for m in matches:
+        team1 = m["players"]["team1"]
+        team2 = m["players"]["team2"]
+        for p in  team1:
+            if p['id'] == int(id):
+                return m
+        for p in team2:
+            if p['id'] == int(id):
+                return m
+
+
+
+#def getCurrentlyLoggedIn():
+ #   r = requests.get("https://new-chapter.eu/app.php/nczone/api/players/logged_in")
+ #   logged_in = r.json();
+ #   for p in logged_in:
+ #       print("Player: " + str(p))
+ #       print("Player Id: " + str(p['id']))
+        
